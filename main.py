0f2e3afab090573fcea5165148fa4400ca236478
Beloded1 <alexandrbeloded@gmail.com>
2023-06-20 23:27:51 +0400

    Add some tests, corrected main program

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import requests
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+from typing import Optional
+from dotenv import load_dotenv
+import os
+from fastapi import HTTPException
 
 
 app = FastAPI()
+load_dotenv()
 
-@app.get("/items/{item_id}")
-def current_weather():
-    city = 'Samara,RU'
-    api_key = '17c8ecf5f173d05e68f31b64cc7b5345'
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-    response = requests.get(url)
-    data = response.json()
-    temperature = data['main']['temp']
-    weather_data = {
-        'city': city,
-        'temperature': temperature,
-    }
 
-    return weather_data
+class Weather(BaseModel):
+    city: str
+    temperature: float
+
+
+@app.get("/weather")
+def current_weather(
+    city: str = Query("Samara,RU", description="City name"),
+    units: Optional[str] = Query("metric", description="Temperature units"),
+):
+    api_key = os.environ['MYAPP_OPEN_WEATHER_KEY']
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units={units}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status() 
+        data = response.json()
+        temperature = data['main']['temp']
+        weather_data = Weather(city=city, temperature=temperature)
+        return weather_data
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=400, detail=str(e)) 
